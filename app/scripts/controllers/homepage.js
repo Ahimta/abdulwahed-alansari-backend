83341fedf9cea3e17110296642e8fb61037ea02d
Abdullah Alansari <ahimta@gmail.com>
2015-08-23 09:34:39 +0300

    feat(route): HomepageCtrl

---
+++ app/scripts/controllers/homepage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abdulwahedAlansariFrontendApp.controller:HomepageCtrl
+ * @description
+ * # HomepageCtrl
+ * Controller of the abdulwahedAlansariFrontendApp
+ */
+angular.module('abdulwahedAlansariFrontendApp')
+  .controller('HomepageCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
