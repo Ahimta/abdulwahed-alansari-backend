c0dd2f66e74eb22e96ea792a1292c11d9dfb3fab
Abdullah Alansari <ahimta@gmail.com>
2015-08-24 11:52:23 +0300

    feat(Angular modules): add angular-loading-bar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'firebase'
+    'firebase',
+    'angular-loading-bar'
   ])
   .config(function ($routeProvider) {
     $routeProvider
