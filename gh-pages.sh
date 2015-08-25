25f64c6790426985a5715a90426db3de0b07bbfb
Abdullah Alansari <ahimta@gmail.com>
2015-08-25 13:05:22 +0300

    feat(automation): deploy to gh-pages with each push

--- gh-pages.sh
+++ gh-pages.sh
@@ -8,7 +8,7 @@ grunt build
 git add --force dist
 git add .
 
-git commit -m "Deploy gh-pages"
+git commit -am "Deploy to gh-pages"
 git push --force "https://${GH_TOKEN}@github.com/Ahimta/abdulwahed-alansari-frontend.git" `git subtree split --prefix dist gh-pages`:gh-pages
 
 git checkout master
