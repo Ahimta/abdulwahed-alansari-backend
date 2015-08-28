240be6e5d8d9e5855889a7df8a8211cc28839564
Abdullah Alansari <ahimta@gmail.com>
2015-08-28 17:32:48 +0300

    chore(maintain): remove unused code in QuotesCtrl

--- app/scripts/controllers/quotes.js
+++ app/scripts/controllers/quotes.js
@@ -8,13 +8,9 @@
  * Controller of the abdulwahedAlansariFrontendApp
  */
 angular.module('abdulwahedAlansariFrontendApp')
-  .controller('QuotesCtrl', function ($routeParams, $location, $firebaseArray, FIREBASE_REF) {
+  .controller('QuotesCtrl', function ($firebaseArray, FIREBASE_REF) {
 
     var quotes = this.quotes = $firebaseArray(FIREBASE_REF.child('quotes'));
-
-    if ($routeParams.action === 'new') { $location.hash('newQuoteButton'); }
-
-    angular.element('#newQuoteModal').on('shown.bs.modal', function () { angular.element('#quoteText').focus(); });
 
     var editing = null;
 
@@ -23,12 +19,6 @@ angular.module('abdulwahedAlansariFrontendApp')
     this.edit = function (quoteId) { editing = quoteId; };
 
     var cancel = this.cancel = function () { editing = null; };
-
-    this.create = function (quote) {
-      quotes.$add(quote);
-      quote.text = '';
-      angular.element('#newQuoteModal').modal('hide');
-    };
 
     this.update = function (quote) {
       cancel();
