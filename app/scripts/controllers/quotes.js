a8ff9059d1a4d50477bac4260e3fc2e0c57b4c68
Abdullah Alansari <ahimta@gmail.com>
2015-08-30 15:26:29 +0300

    feat(views): use CKEditor where appropriate

--- app/scripts/controllers/quotes.js
+++ app/scripts/controllers/quotes.js
@@ -10,21 +10,8 @@
 angular.module('abdulwahedAlansariFrontendApp')
   .controller('QuotesCtrl', function ($firebaseArray, FIREBASE_REF, UserService) {
 
-    var quotes = this.quotes = $firebaseArray(FIREBASE_REF.child('quotes'));
+    this.quotes = $firebaseArray(FIREBASE_REF.child('quotes'));
 
     this.isVisitor = UserService.isVisitor;
     this.isAdmin   = UserService.isAdmin;
-
-    var editing = null;
-
-    this.isEditing = function (quoteId) { return editing === quoteId; };
-
-    this.edit = function (quoteId) { editing = quoteId; };
-
-    var cancel = this.cancel = function () { editing = null; };
-
-    this.update = function (quote) {
-      cancel();
-      quotes.$save(quote);
-    };
   });
