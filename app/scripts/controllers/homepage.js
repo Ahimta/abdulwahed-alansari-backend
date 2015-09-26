bdf102419ed95a115028c63d88e764e655a4ca39
Abdullah Alansari <ahimta@gmail.com>
2015-09-26 16:51:47 +0300

    feat(assets): include CKEditor only in about page

--- app/scripts/controllers/homepage.js
+++ app/scripts/controllers/homepage.js
@@ -8,5 +8,10 @@
  * Controller of the abdulwahedAlansariFrontendApp
  */
 angular.module('abdulwahedAlansariFrontendApp')
-  .controller('HomepageCtrl', function () {
+  .controller('HomepageCtrl', function ($firebaseArray, FIREBASE_REF, UserService) {
+
+    this.articles = $firebaseArray(FIREBASE_REF.child('articles'));
+
+    this.isVisitor = UserService.isVisitor;
+    this.isAdmin   = UserService.isAdmin;
   });
