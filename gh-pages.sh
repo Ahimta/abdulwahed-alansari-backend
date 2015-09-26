d4897cec1d8d3628d9fd7cbd901a5ac7a4d82bf4
Abdullah Alansari <ahimta@gmail.com>
2015-09-26 16:09:40 +0300

    remove: deploying to GitHub Pages from gh-pages.sh

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,11 +5,8 @@ git checkout -b gh-pages HEAD
 
 grunt build
 
-git config user.name "Travis CI"
-git config user.email "ahimta@gmail.com"
-
 git add --force dist
 git add .
 
 git commit -am "Deploy to gh-pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/Ahimta/abdulwahed-alansari-frontend.git" `git subtree split --prefix dist gh-pages`:gh-pages > /dev/null 2>&1
+git push --force origin `git subtree split --prefix dist gh-pages`:gh-pages
