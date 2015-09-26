7d361726e3329aef96963a838b776918ba425e02
Abdullah Alansari <ahimta@gmail.com>
2015-09-26 17:29:19 +0300

    fix(GitHub Pages): use --all when using 'git add'

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,8 +5,7 @@ git checkout -b gh-pages HEAD
 
 grunt build
 
-git add --force dist
-git add .
+git add --force --all dist
 
 git commit -am "Deploy to gh-pages"
 git push --force origin `git subtree split --prefix dist gh-pages`:gh-pages
