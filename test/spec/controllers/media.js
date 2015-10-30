b32af071786f87e7a8324cbe3cc40d07995eab2f
Abdullah Alansari <ahimta@gmail.com>
2015-10-30 09:09:02 +0300

    feat(travis): media.js spec fails

--- test/spec/controllers/media.js
+++ test/spec/controllers/media.js
@@ -18,6 +18,6 @@ describe('Controller: MediaCtrl', function () {
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(!!MediaCtrl.awesomeThings).toBe(true);
+    expect(!!MediaCtrl).toBe(true);
   });
 });
